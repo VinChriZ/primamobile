9c114d732491ff224d87c5ef53363fe04a1012a1
VinChriZ <vinchris023@gmail.com>
2024-12-19 08:37:01 +0700

    add device_preview and flutter_bloc depedencies, update app and app_router

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,26 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:primamobile/app/app_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'myNPTI',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        fontFamily: 'Montserrat',
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue),
+        primarySwatch: Colors.green,
       ),
+      initialRoute: '/',
+      onGenerateRoute: AppRouter.onGenerateRoutes,
+      debugShowCheckedModeBanner: false,
+      builder: DevicePreview.appBuilder,
+      // useInheritedMediaQuery: true, //-
+      // locale: DevicePreview.locale(context), //-
     );
   }
 }
---
+++ lib/app/app_router.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:mynpti_mv/app/bloc/authentication_bloc.dart';
+// import 'package:mynpti_mv/app/pages.dart';
+
+class AppRouterMissingArgumentException implements Exception {
+  final String message = "Missing route parameters.";
+  @override
+  String toString() {
+    return "Exception: $message";
+  }
+}
+
+class AppRouter {
+  static Route<dynamic> onGenerateRoutes(RouteSettings settings) {
+    // AuthenticationBloc authenticationBloc = AuthenticationBloc()
+    //   ..add(AppStarted());
+    switch (settings.name) {
+      case '/':
+      case '/splash':
+        return MaterialPageRoute(builder: (_) => const Placeholder());
+      // case '/login':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const LoginPage(),
+      //     ),
+      //   );
+      // case '/home':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const HomePage(),
+      //     ),
+      //   );
+      // case '/account':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const AccountPage(),
+      //     ),
+      //   );
+      // case '/profile':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const ProfilePage(),
+      //     ),
+      //   );
+      // case '/settings':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const SettingsPage(),
+      //     ),
+      //   );
+      // case '/change_password':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const ChangePasswordPage(),
+      //     ),
+      //   );
+      // case '/shift':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const ShiftPage(),
+      //     ),
+      //   );
+      // case '/shift_calendar':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider.value(
+      //       value: authenticationBloc,
+      //       child: const ShiftCalendarPage(),
+      //     ),
+      //   );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Page not found')),
+          ),
+        );
+    }
+  }
+}
